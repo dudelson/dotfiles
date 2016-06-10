96b3f53fabbca908a07f9269835f45600d8eaf57
David Udelson <dru5@cornell.edu>
2016-06-10 09:54:28 -0400

    Removed some tiling layouts that I never use from rc.lua

--- xorg+awesome/rc.lua
+++ xorg+awesome/rc.lua
@@ -75,11 +75,11 @@ local layouts =
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
+    --awful.layout.suit.spiral,
+    --awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    --awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.magnifier
 }
 -- }}}
 
