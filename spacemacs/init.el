ce4de4cb770d490a7709c1ec93041c9cf8770521
David Udelson <dru5@cornell.edu>
2016-06-27 09:19:06 -0400

    Added "shell-scripts" layer to init.el

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -41,6 +41,7 @@ values."
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
+     shell-scripts
      ;; spell-checking
      syntax-checking
      unimpaired
