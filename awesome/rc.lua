36bd1bdd4bd1143c24fe150f444d802c72e6527b
David Udelson <dru5@cornell.edu>
2017-05-24 11:30:05 -0400

    Add awesome custom theme
    The wibar has been restyled based on the multicolor theme by @copycat-killer.
    This provided an opportunity to move a lot of the styling configuration,
    including the creation of the wibar and widgets, to a custom theme.
    Additionally, my homegrown sysmonitor and update_notifications widgets have been
    replaced by vicious widgets; accordingly, there is now a dependency on the
    vicious library, which has been included as a submodule. Using the vicious pkg
    widget somehow solves the async issues I have been having with
    update_notifications for so long. Using a custom theme also sets the wallpaper
    automatically, so `sh ~/.fehbg` is no longer required. Lastly, descriptions have
    been added to several keybindings so that they show up in the new hotkeys help menu.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -2,29 +2,18 @@
 print("-=-=- RESTART " .. os.date("%a %b %d %X") .. " -=-=-")
 
 -- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
+local gears     = require("gears")
+local awful     = require("awful")
+                  require("awful.autofocus")
+local wibox     = require("wibox")
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
+local naughty   = require("naughty")
+
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 -- Enable VIM help for hotkeys widget when client with matching name is opened:
 require("awful.hotkeys_popup.keys.vim")
 -- alt+tab functionality
 local alttab = require("alttab")
--- homegrown system information widget
-local sysmonitor = require("sysmonitor")
--- homegrown update notifications widget
-local update_notifications = require("update_notifications")
--- calendar widget from wiki
-local cal = require("cal")
--- homegrown caffeine widget
-local caffeine = require("caffeine")
 -- yaml library for parsing my config
 local yaml = require("yaml")
 
@@ -53,22 +42,33 @@ do
 end
 -- }}}
 
+-- {{{ Helper functions
+local function client_menu_toggle_fn()
+  local instance = nil
+
+  return function ()
+    if instance and instance.wibox.visible then
+      instance:hide()
+      instance = nil
+    else
+      instance = awful.menu.clients({ theme = { width = 250 } })
+    end
+  end
+end
+-- }}}
+
 -- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+local theme_path = string.format("%s/dotfiles/awesome/custom_theme/theme.lua", os.getenv("HOME"))
+beautiful.init(theme_path)
 
--- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
+local modkey     = "Mod4"
+local altkey     = "Mod1"
+local terminal   = "urxvt"
+local editor     = os.getenv("EDITOR") or "nano"
+local editor_cmd = terminal .. " -e " .. editor
+local browser    = "firefox"
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
-
+awful.util.tagnames = { "term", "web", "spc", "irc" }
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.floating,
@@ -89,59 +89,6 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.se,
 }
 
--- read custom configuration yaml
-local dudelson_config = yaml.loadpath("/home/david/.config/dotfiles.yml")
--- }}}
-
-
--- {{{ Helper functions
-local function client_menu_toggle_fn()
-    local instance = nil
-
-    return function ()
-        if instance and instance.wibox.visible then
-            instance:hide()
-            instance = nil
-        else
-            instance = awful.menu.clients({ theme = { width = 250 } })
-        end
-    end
-end
--- }}}
-
--- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-   { "hotkeys", function() return false, hotkeys_popup.show_help end },
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end}
-}
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
-mytextclock:set_font("source code pro 9")
--- add calendar widget to clock
-cal.register(mytextclock)
-
--- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
                     awful.button({ }, 1, function(t) t:view_only() end),
                     awful.button({ modkey }, 1, function(t)
@@ -183,71 +130,51 @@ local tasklist_buttons = gears.table.join(
                      awful.button({ }, 5, function ()
                                               awful.client.focus.byidx(-1)
                                           end))
+-- }}}
 
+-- read custom configuration yaml
+local dudelson_config = yaml.loadpath("/home/david/.config/dotfiles.yml")
+-- }}}
+
+-- {{{ Menu
+-- Create a launcher widget and a main menu
+myawesomemenu = {
+   { "hotkeys", function() return false, hotkeys_popup.show_help end },
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", function() awesome.quit() end}
+}
+
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "open terminal", terminal }
+                                  }
+                        })
+
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+                                     menu = mymainmenu })
+
+-- Keyboard map indicator and switcher
+mykeyboardlayout = awful.widget.keyboardlayout()
+
+-- {{{ Screen 
 local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
+  -- Wallpaper
+  if beautiful.wallpaper then
+    local wallpaper = beautiful.wallpaper
+    -- If wallpaper is a function, call it with the screen
+    if type(wallpaper) == "function" then
+      wallpaper = wallpaper(s)
     end
+    gears.wallpaper.maximized(wallpaper, s, true)
+  end
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
 
-awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
-    -- Each screen has its own tag table.
-    awful.tag({ "term", "web", "spc", "irc", "5", "6", "7"}, s, awful.layout.layouts[1])
-
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
-
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
-        },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            -- add my homegrown sysmonitor, caffiene, and update notifications widgets
-            sysmonitor.widget,
-            caffeine.widget,
-            update_notifications.widget,
-            mykeyboardlayout,
-            wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
-        },
-    }
-end)
+-- Create a wibox for each screen and add it
+awful.screen.connect_for_each_screen(function (s) beautiful.at_screen_connect(s) end)
 -- }}}
 
 -- {{{ Mouse bindings
@@ -298,13 +225,14 @@ end
 ----------------- keybindings that don't depend on configuration ---------------
 globalkeys = gears.table.join(
     -- Movement between tags
-    awful.key({ modkey,           }, "c",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "v",   awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "z", awful.tag.history.restore),
-
+    awful.key({ modkey,           }, "c", function () awful.tag.viewprev(awful.screen.focused()) end,
+              {description = "view next tag", group = "tag"}),
+    awful.key({ modkey,           }, "v", function () awful.tag.viewnext(awful.screen.focused()) end,
+              {description = "view previous tag", group = "tag"}),
+    awful.key({ modkey,           }, "z", awful.tag.history.restore,
+              {description = "jump to last most recent tag", group = "tag"}),
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
-
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
               {description = "swap with next client by index", group = "client"}),
@@ -377,57 +305,58 @@ globalkeys = gears.table.join(
                   }
               end,
               {description = "lua execute prompt", group = "awesome"}),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-      {description = "show the menubar", group = "launcher"}),
-    -- show hotkeys help
-    awful.key({ modkey,           }, "h",      hotkeys_popup.show_help,
-      {description="show help", group="awesome"}),
     -- pause sysmonitor stats updates
-    awful.key({ modkey },            "s",     sysmonitor.toggle_freeze ),
+    -- awful.key({ modkey },            "s",     beautiful.sysmonitor.toggle_freeze
+    --   {description="toggle pause sysmonitor", group="misc"}),
     -- Alt+tab
     awful.key({"Mod1",          }, "Tab",
-	function ()
-	    alttab.switch(1, "Alt_L", "Tab", "ISO_Left_Tab")
-	end
+              function () alttab.switch(1, "Alt_L", "Tab", "ISO_Left_Tab") end,
+              {description = "alt-tab", group = "client"}
     ),
-
     awful.key({"Mod1", "Shift"  }, "Tab",
-	function()
-	    alttab.switch(-1, "Alt_L", "Tab", "ISO_Left_Tab")
-	end
+              function() alttab.switch(-1, "Alt_L", "Tab", "ISO_Left_Tab") end,
+              {description = "reverse alt-tab", group = "client"}
     ),
     -- Printscreen
     awful.key({                 }, "Print",
-	function()
-	    awful.util.spawn(os.date('maim /tmp/screenshot_%Y-%m-%d_%X.png'), false)
-	end
+              function()
+	            awful.spawn(os.date('maim /tmp/screenshot_%Y-%m-%d_%X.png'), false)
+	          end,
+              {description = "take a screenshot", group = "misc"}
     ),
 
     -- Keybindings to focus specific tags
     -- "Mod1" is left alt
     -- jump to terminal tag
-    awful.key({ modkey, "Mod1"   }, "j", function ()
-        -- awful.tag.viewonly(tags[apps.terminal.screen][apps.terminal.tag])
-        -- awful.screen.focus(apps.terminal.screen)
-        awful.tag.find_by_name(awful.screen.focused(), "term"):view_only()
-    end),
+    awful.key({ modkey, "Mod1"   }, "j",
+              function ()
+                -- awful.tag.viewonly(tags[apps.terminal.screen][apps.terminal.tag])
+                -- awful.screen.focus(apps.terminal.screen)
+                awful.tag.find_by_name(awful.screen.focused(), "term"):view_only()
+              end,
+              {description = "jump to term tag", group = "tag"}),
     -- jump to browser tag
-    awful.key({ modkey, "Mod1"   }, "k", function ()
-        -- awful.tag.viewonly(tags[apps.browser.screen][apps.browser.tag])
-        -- awful.screen.focus(apps.browser.screen)
-        awful.tag.find_by_name(awful.screen.focused(), "web"):view_only()
-    end),
+    awful.key({ modkey, "Mod1"   }, "k",
+              function ()
+                -- awful.tag.viewonly(tags[apps.browser.screen][apps.browser.tag])
+                -- awful.screen.focus(apps.browser.screen)
+                awful.tag.find_by_name(awful.screen.focused(), "web"):view_only()
+              end,
+              {description = "jump to web tag", group = "tag"}),
     -- jump to spacemacs tag
-    awful.key({ modkey, "Mod1"   }, "l", function ()
-        -- local tag = dudelson_config.application_layout.emacs[2]
-        -- awful.tag.viewonly(tags[apps.emacs.screen][apps.emacs.tag])
-        -- awful.screen.focus(apps.emacs.screen)
-        awful.tag.find_by_name(awful.screen.focused(), "spc"):view_only()
-    end),
-    awful.key({ modkey, "Mod1"   }, ";", function ()
-        awful.tag.find_by_name(awful.screen.focused(), "irc"):view_only()
-    end)
+    awful.key({ modkey, "Mod1"   }, "l",
+              function ()
+                -- local tag = dudelson_config.application_layout.emacs[2]
+                -- awful.tag.viewonly(tags[apps.emacs.screen][apps.emacs.tag])
+                -- awful.screen.focus(apps.emacs.screen)
+                awful.tag.find_by_name(awful.screen.focused(), "spc"):view_only()
+              end,
+              {description = "jump to spacemacs tag", group = "tag"}),
+    awful.key({ modkey, "Mod1"   }, ";",
+              function ()
+                awful.tag.find_by_name(awful.screen.focused(), "irc"):view_only()
+              end,
+              {description = "jump to irc tag", group = "tag"})
 )
 
 -- Add the configuration-dependent keybingings to the global keybinding table
@@ -640,6 +569,8 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- auto-jump to urgent windows
+client.connect_signal("property::urgent", function(c) awful.client.urgent.jumpto() end)
 -- }}}
 
 
